acb73dadb033172b6f8c15cb5a8c1874597d255b
Khanh Nguyen <takashilouisnguyen@gmail.com>
2024-07-31 20:13:49 -0500

    update CD workflow

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-docker rm -f website
+docker rm -f website &2>/dev/null || true
 docker images -- format '{{.Repository}}:{{.Tag}}' | grep 'takashilouisnguyen/crawler-website' | xargs -r docker rmi
 docker run --net crawler --name website -p 3100:80 takashilouisnguyen/crawler-website:$1
